126e10a07181c3e83ad1155a60775b3d7e9d9245
Shibaitle <shiba.despacito@gmail.com>
2026-02-28 23:52:05 +0700

    createsuperuser without shell in render

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+# build.sh (ตรวจสอบว่ามีไฟล์นี้อยู่ในโปรเจกต์)
+
+#!/usr/bin/env bash
+
+# Exit on error
+
+set -o errexit
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --noinput || true
